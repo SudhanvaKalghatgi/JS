68eec031861eb24bb45149e3cc6c932c67088ecb
SudhanvaKalghatgi <sudhanvakalghatgi3@gmail.com>
2025-08-12 07:42:03 +0000

    config done

---
+++ basics_2/arrays2.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros] // spread operator
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) // resolves array ke andar array and gives us a final array
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh")) // converts a non array element into array
+console.log(Array.from({name: "hitesh"})) // interesting[doesn't convert because we have to specify what to convert into array]
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // converts to array
