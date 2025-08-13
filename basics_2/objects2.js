dd66b56d9d545364fdd9f06bdeaa412389d6513b
SudhanvaKalghatgi <sudhanvakalghatgi3@gmail.com>
2025-08-13 08:21:44 +0000

    config done

---
+++ basics_2/objects2.js
@@ -0,0 +1,81 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "sudhanva",
+            lastname: "kalghatgi"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // basically the empty parenthesis show that it is a new target object and all the values are stored into it.
+
+const obj3 = {...obj1, ...obj2} // spread operator
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+ console.log(Object.keys(tinderUser)); // returns the keys within the object
+ console.log(Object.values(tinderUser)); // retuens the values within the object
+ console.log(Object.entries(tinderUser)); // returns array which contains sub arrays with each keys and its values
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "sudhanva"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "sudhanva",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
