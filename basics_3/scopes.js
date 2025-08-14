91761a952858234599aa7b55a08099f5c9b0be5f
SudhanvaKalghatgi <sudhanvakalghatgi3@gmail.com>
2025-08-14 05:48:51 +0000

    config done

---
+++ basics_3/scopes.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "sudhanva"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "ssudhanva"
+    if (username === "sudhanva") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) // no problem in declaring the function above wouldn't disturb the code
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // here function is assigned to a variable name so it doesn't carry the execution
+const addTwo = function(num){
+    return num + 2
+}
