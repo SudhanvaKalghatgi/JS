db2b37af3935f9e70b59f43f0ae9ac2e77fe9970
SudhanvaKalghatgi <sudhanvakalghatgi3@gmail.com>
2025-08-18 14:13:52 +0000

    edited

--- basics_2/arrays2.js
+++ basics_2/arrays2.js
@@ -1,7 +1,7 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
@@ -11,12 +11,12 @@ const dc_heros = ["superman", "flash", "batman"]
 
 const all_new_heros = [...marvel_heros, ...dc_heros] // spread operator
 
-// console.log(all_new_heros);
+//  console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity) // resolves array ke andar array and gives us a final array
-console.log(real_another_array);
+// const real_another_array = another_array.flat(Infinity) // resolves array ke andar array and gives us a final array
+// console.log(real_another_array);
 
 
 
@@ -24,8 +24,8 @@ console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh")) // converts a non array element into array
 console.log(Array.from({name: "hitesh"})) // interesting[doesn't convert because we have to specify what to convert into array]
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1, score2, score3)); // converts to array
+// console.log(Array.of(score1, score2, score3)); // converts to array
